306a11b56d225401069c5cbd9ea36e41ac144340
Ryan Searls <Ryan.searls@gmail.com>
2021-06-14 14:34:27 -0500

    Made API with 3 paths

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,16 @@
 class ExamplePagesController < ApplicationController
+  def hello_method
+    render json: {message: "Hello"}
+  end
+  
+  def goodbye_method
+    render json: {message: "goodbye"}
+  end  
+
+  def hi_method
+    render json: [{ fruit:"strawberry", color: "red"} , {vegetable: "peas", color: "green"}]
+
+  end   
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/hello_path", controller: "example_pages", action: "hello_method"
+
+  get "/goodbye_path", controller: "example_pages", action: "goodbye_method"
+
+  get "/hi_path", controller: "example_pages", action: "hi_method"
+
 end
+
